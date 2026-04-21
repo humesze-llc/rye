42f5bfd80bcaf7fb2e3673b9318951caff4c40f8
Zach Humes <humesze@proton.me>
2026-04-21 00:09:48 -0500

    feat(rye): re-export HyperbolicH3 and Iso3H from prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,5 +1,5 @@
 pub use rye_asset::{AssetEvent, AssetEventKind, AssetWatcher};
-pub use rye_math::{EuclideanR3, Iso3, Space, Tangent, WgslSpace};
+pub use rye_math::{EuclideanR3, HyperbolicH3, Iso3, Iso3H, Space, Tangent, WgslSpace};
 pub use rye_render::*;
 pub use rye_shader::{ShaderDb, ShaderId};
 pub use rye_time::FixedTimestep;

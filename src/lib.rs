68d5b67e445f22d5ae120f05560b10413c303e01
Zach Humes <humesze@proton.me>
2026-04-23 16:10:59 -0500

    chore: delete empty stub crates rye-core, rye-scenes, rye-window

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,7 @@
 pub use rye_asset as asset;
-pub use rye_core as core;
 pub use rye_math as math;
 pub use rye_render as render;
-pub use rye_scenes as scenes;
 pub use rye_shader as shader;
 pub use rye_time as time;
-pub use rye_window as window;
 
 pub mod prelude;
